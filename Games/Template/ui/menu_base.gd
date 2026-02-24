8f416a342234bd624f7be828e4872eacfa3344f6
CreativeRyu <creativeryu@web.de>
2026-02-24 22:59:53 +0100

    refactor(ui): introduce MenuBase and centralize focus, input and button locking

---
+++ Games/Template/ui/menu_base.gd
@@ -0,0 +1,51 @@
+class_name MenuBase
+extends Control
+
+signal pressed_action
+
+@export var default_focus_path : NodePath
+
+var is_button_locked := false
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
+func on_opened():
+	is_button_locked = false
+	await get_tree().process_frame
+
+	if not default_focus_path.is_empty():
+		var node = get_node_or_null(default_focus_path)
+
+		if node and node.has_method("grab_focus"):
+			node.grab_focus()
+		else:
+			_focus_first_button()
+
+func _focus_first_button():
+	var buttons = find_children("*", "BaseButton")
+	if buttons.size() > 0:
+		buttons[0].grab_focus()
+
+func _unhandled_input(event):
+	if not is_visible_in_tree():
+		return
+	if event.is_action_pressed("ui_accept", true):
+		_on_accept_pressed()
+	if event.is_action_pressed("ui_cancel", true):
+		_on_cancel_pressed()
+
+func _on_accept_pressed():
+	var focus = get_viewport().gui_get_focus_owner()
+	if focus and focus is BaseButton and is_ancestor_of(focus):
+		focus.pressed.emit()
+
+func _on_cancel_pressed():
+	pass
+
+func emit_action(signal_to_emit : Signal):
+	if is_button_locked:
+		return
+	is_button_locked = true
+	signal_to_emit.emit()
+	pressed_action.emit()
--- Games/Template/ui/pause_menu.gd
+++ Games/Template/ui/pause_menu.gd
@@ -1,42 +1,17 @@
-extends Control
+extends MenuBase
 
 signal resume_pressed
 signal restart_pressed
 signal back_to_start_pressed
-signal pressed_action
-
-@onready var resume_button = $VBoxContainer/ResumeButton
-
-var is_button_locked: bool = false
-
-func _ready():
-	process_mode = Node.PROCESS_MODE_ALWAYS
-	
-func on_opened():
-	is_button_locked = false
-	
-	await get_tree().process_frame
-	resume_button.grab_focus()
-
-func _emit_action(action: Signal):
-	if is_button_locked:
-		return
-	is_button_locked = true
-		
-	action.emit()
-	pressed_action.emit()
-
-func _unhandled_input(event):
-	if not visible:
-		return
-	if event.is_action_pressed("ui_accept", true) and resume_button.has_focus():
-		_emit_action(resume_pressed)
 
 func _on_resume_button_pressed():
-	_emit_action(resume_pressed)
+	emit_action(resume_pressed)
 
 func _on_back_button_pressed():
-	_emit_action(back_to_start_pressed)
+	emit_action(back_to_start_pressed)
 
 func _on_restart_button_pressed() -> void:
-	_emit_action(restart_pressed)
+	emit_action(restart_pressed)
+
+func _on_cancel_pressed():
+	emit_action(resume_pressed)
